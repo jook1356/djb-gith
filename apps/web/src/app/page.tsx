ab0e95154ca22d32034f47429851091f6603711a
DongJuKim-Carlo <147456232+jook1356a@users.noreply.github.com>
2025-09-10 14:57:44 +0900

    1

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,8 +1,14 @@
 import "./page.scss";
 import Header from "@/components/Header/Header";
+import React from "react";
 
 export default function Home() {
   return (
-    <Header />
+    <React.Fragment>
+      <Header />
+      <div style={{ height: "100vh" }}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </div>
+    </React.Fragment>
   );
 }
